faa2dcc4f383baaa20e46d5afbb5bcb3d064b67b
Dmytro Velychko <dmytro.velychko@gmail.com>
2024-03-01 12:36:10 +0100

    Formatting

--- train-bars.py
+++ train-bars.py
@@ -21,70 +21,69 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #device = "cpu"
 
 
-if __name__ == "__main__":
-    if False:
-        seed = 123456
-        random.seed(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-    
+if False:
+    seed = 123456
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
-    N = 1000
-    H = 2*5  # number of sources
-    D = int(H / 2)**2  # data dimensionality
-    dataset = BarsDataset(N, D, H, latent="Laplace")
-    #H += 2  # additional letent dimensions
-    
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1000)
-    
-    likelihood_entropies = []
-    prior_entropies = []
-    proposal_entropies = []
-    def on_compute_entropy(likelihood_entropy, prior_entropy, proposal_entropy):
-        likelihood_entropies.append(likelihood_entropy.item())
-        prior_entropies.append(prior_entropy.item())
-        proposal_entropies.append(proposal_entropy.item())
 
-    model = OptimalManifoldSC(N, D, H, 
-            variationalparams=FullCovarGaussianVariationalParams(N, D, H), 
-            on_compute_entropy=on_compute_entropy).to(device)
-    
-    path = "./out/bars/" + model.__class__.__name__ + "/" + datetime.now().strftime('%y.%m.%d-%H:%M:%S') + "/"
-    if not os.path.exists(path):
-        os.makedirs(path)
-    prefix = path
+N = 1000
+H = 2*5  # number of sources
+D = int(H / 2)**2  # data dimensionality
+dataset = BarsDataset(N, D, H, latent="Laplace")
+#H += 2  # additional letent dimensions
 
-    plot_training_data(dataset.x[:H], filename=prefix + "training-data.pdf")
-    
-    def save_plots(model, epoch, elbos=None):
-        if epoch % 10 == 0:
-            plot(elbos, filename=prefix + "elbos.pdf")
-            plot_Ws(model, filename=prefix + "Ws-{:05}.pdf".format(epoch))
-        
-        if epoch % 10 == 0:
-            indexes = range(10)
-            x = dataset.x[indexes]
-            plot_samples(model, x, indexes, filename=prefix + "X-{:05}.pdf".format(epoch))
-    
-    save_plots(model, epoch=0)
-    if isinstance(model, (OptimalManifoldSC, AnalyticELBOSC)):
-        model.prior_scale = 1
-        elbos, gradsizes = train_LBFGS(model, dataloader, nepochs=1000, on_epoch=save_plots, 
-            history_size=200, 
-           max_iter=200)
-        save_plots(model, 10000)
-        plt.plot(gradsizes)
-        plt.gca().set_yscale("log")
-        plt.title("gradient size")
-        plt.savefig(prefix + "gradsizes.pdf")
-        plt.close()
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=1000)
 
-        # Set ground-truth W and compute max ELBO
-        with torch.no_grad():
-            model.W.copy_(torch.Tensor(dataset.gen.W))
-        data = dataset.x.type(torch.get_default_dtype()).to(model.device)
-        gt_elbo = optimize_latents(model, data, indexes=range(len(data)), init=False, max_iter=200)
-        print("Ground truth ELBO: {}".format(gt_elbo))
-        np.savetxt(prefix + "groundtruth-elbo.txt", np.array([gt_elbo]), fmt="%f", delimiter=",")
+likelihood_entropies = []
+prior_entropies = []
+proposal_entropies = []
+def on_compute_entropy(likelihood_entropy, prior_entropy, proposal_entropy):
+    likelihood_entropies.append(likelihood_entropy.item())
+    prior_entropies.append(prior_entropy.item())
+    proposal_entropies.append(proposal_entropy.item())
+
+model = OptimalManifoldSC(N, D, H, 
+        variationalparams=FullCovarGaussianVariationalParams(N, D, H), 
+        on_compute_entropy=on_compute_entropy).to(device)
+
+path = "./out/bars/" + model.__class__.__name__ + "/" + datetime.now().strftime('%y.%m.%d-%H:%M:%S') + "/"
+if not os.path.exists(path):
+    os.makedirs(path)
+prefix = path
+
+plot_training_data(dataset.x[:H], filename=prefix + "training-data.pdf")
+
+def save_plots(model, epoch, elbos=None):
+    if epoch % 10 == 0:
+        plot(elbos, filename=prefix + "elbos.pdf")
+        plot_Ws(model, filename=prefix + "Ws-{:05}.pdf".format(epoch))
+    
+    if epoch % 10 == 0:
+        indexes = range(10)
+        x = dataset.x[indexes]
+        plot_samples(model, x, indexes, filename=prefix + "X-{:05}.pdf".format(epoch))
+
+save_plots(model, epoch=0)
+if isinstance(model, (OptimalManifoldSC, AnalyticELBOSC)):
+    model.prior_scale = 1
+    elbos, gradsizes = train_LBFGS(model, dataloader, nepochs=1000, on_epoch=save_plots, 
+        history_size=200, 
+        max_iter=200)
+    save_plots(model, 10000)
+    plt.plot(gradsizes)
+    plt.gca().set_yscale("log")
+    plt.title("gradient size")
+    plt.savefig(prefix + "gradsizes.pdf")
+    plt.close()
+
+    # Set ground-truth W and compute max ELBO
+    with torch.no_grad():
+        model.W.copy_(torch.Tensor(dataset.gen.W))
+    data = dataset.x.type(torch.get_default_dtype()).to(model.device)
+    gt_elbo = optimize_latents(model, data, indexes=range(len(data)), init=False, max_iter=200)
+    print("Ground truth ELBO: {}".format(gt_elbo))
+    np.savetxt(prefix + "groundtruth-elbo.txt", np.array([gt_elbo]), fmt="%f", delimiter=",")
 
 
